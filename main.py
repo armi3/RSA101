a9e48328a2d32532711fb3832f18147cba6d7d6c
Fernanda González <armi3@protonmail.ch>
2018-11-17 21:05:02 -0600

    File reading and writing.

--- main.py
+++ main.py
@@ -1,56 +1,171 @@
 import os, sys
 sys.path.insert(0, './modules')
 import assets
+import rsa_algorithm as calc
 
 
 if __name__ == '__main__':
     print assets.banner
-    #print assets.binary_exponentiation(123, 1001, 101)
     loop = True
     while loop:
-        print assets.menu1
+        print assets.menu
         choice = input("Enter your choice [1-5]: ")
+#=============================================================================== 
         if choice == 1:
-            pub, priv = assets.keypair(61,53)
-            print pub, priv
-#===============================================================================            
-            message = raw_input("Enter a msg to encrypt with your pub key: ")
-            #to_encrypt = [str(ord(char)).zfill(3) for char in message]
-            to_encrypt = [ord(char) for char in message]
-            #to_encrypt = map(int, to_encrypt)
-            print to_encrypt
-            
-            cipher =[]
-            for i in range(len(to_encrypt)):
-                cipher.append(assets.encrypt(priv, to_encrypt[i]))
-            print cipher
-            
-            print map(int, cipher)
-            #print ''.join(map(lambda x: str(x), cipher))
-            #print to_encrypt
+            p = int(raw_input("Enter a prime number (17, 19, 23, etc): "))
+            q = int(raw_input("Enter another prime number (Not one you entered above): "))
+            print "Generating your public/private keypairs now . . ."
+            pub, priv = calc.keypair(61,53)
+            print "Your public key is: ", pub, " and your private key is: ", priv
+            raw_input("Press enter to continue to main menu.")          
 #===============================================================================   
-            cipher = raw_input("Enter a cipher to decrypt with your private key: ")
-            cipher = cipher.split(", ")
-            cipher = [s.strip('[') for s in cipher]
-            cipher = [s.strip(']') for s in cipher]
-            cipher = map(int,cipher)
-            print cipher
-            print type(cipher)
-            
-            to_decrypt = []
-            for i in range(len(cipher)):
-                to_decrypt.append(assets.decrypt(pub, cipher[i]))
-            print to_decrypt
-            
-            print ''.join(map(chr,map(int, to_decrypt)))
-        
         elif choice == 2:
-            loop = False
+            try:
+                pub
             
+            except NameError:
+                p = int(raw_input("Enter a prime number (17, 19, 23, etc): "))
+                q = int(raw_input("Enter another prime number (Not one you entered above): "))
+                print "Generating your public/private keypairs now . . ."
+                pub, priv = calc.keypair(61,53)
+                print "Your public key is: ", pub, " and your private key is: ", priv
+
+                message = raw_input("Enter a msg to encrypt with your pub key: ")
+                to_encrypt = [ord(char) for char in message]
+                print "The ascii representation of your message is:", to_encrypt
+                
+                cipher =[]
+                for i in range(len(to_encrypt)):
+                    cipher.append(calc.encrypt(priv, to_encrypt[i]))
+                print "Your encrypted cipher is:", cipher
+                raw_input("Press enter to continue to main menu.")  
+            
+            else:
+                message = raw_input("Enter a msg to encrypt with your pub key: ")
+                to_encrypt = [ord(char) for char in message]
+                print "The ascii representation of your message is:", to_encrypt
+                
+                cipher =[]
+                for i in range(len(to_encrypt)):
+                    cipher.append(calc.encrypt(priv, to_encrypt[i]))
+                print "Your encrypted cipher is:", cipher
+                raw_input("Press enter to continue to main menu.")  
 #===============================================================================
-# xx=[str(ord(char)).zfill(3) for char in "a&%"]
-# print ''.join(map(lambda x: str(x), xx))
-# [cipher[i:i+3] for i in range(0, len(cipher), 3)]
-# listanum = map(int, listacipher)
-# ''.join(map(chr,listanum))
+        elif choice == 3:
+            try:
+                pub
+            
+            except NameError:
+                p = int(raw_input("Enter a prime number (17, 19, 23, etc): "))
+                q = int(raw_input("Enter another prime number (Not one you entered above): "))
+                print "Generating your public/private keypairs now . . ."
+                pub, priv = calc.keypair(61,53)
+                print "Your public key is: ", pub, " and your private key is: ", priv
+                
+                cipher = raw_input("Enter a cipher to decrypt with your private key: ")
+                cipher = cipher.split(", ")
+                cipher = [s.strip('[') for s in cipher]
+                cipher = [s.strip(']') for s in cipher]
+                cipher = map(int,cipher)
+                
+                to_decrypt = []
+                for i in range(len(cipher)):
+                    to_decrypt.append(calc.decrypt(pub, cipher[i]))
+                print "The ascii representation of your decrypted cipher is:", to_decrypt
+                print "Your decrypted cipher is:", ''.join(map(chr,map(int, to_decrypt)))
+                raw_input("Press enter to continue to main menu.")  
+
+            else:
+                cipher = raw_input("Enter a cipher to decrypt with your private key: ")
+                cipher = cipher.split(", ")
+                cipher = [s.strip('[') for s in cipher]
+                cipher = [s.strip(']') for s in cipher]
+                cipher = map(int,cipher)
+                
+                to_decrypt = []
+                for i in range(len(cipher)):
+                    to_decrypt.append(calc.decrypt(pub, cipher[i]))
+                print "The ascii representation of your decrypted cipher is:", to_decrypt
+                print "Your decrypted cipher is:", ''.join(map(chr,map(int, to_decrypt)))
+                raw_input("Press enter to continue to main menu.")  
 #===============================================================================
+        elif choice == 4:
+            try:
+                pub
+            
+            except NameError:
+                p = int(raw_input("Enter a prime number (17, 19, 23, etc): "))
+                q = int(raw_input("Enter another prime number (Not one you entered above): "))
+                print "Generating your public/private keypairs now . . ."
+                pub, priv = calc.keypair(61,53)
+                print "Your public key is: ", pub, " and your private key is: ", priv
+                
+                message = open("input.txt", "r").read()
+                print "Your file's message to encrypt is: \n", message
+                to_encrypt = [ord(char) for char in message]
+                print "The ascii representation of your message is:", to_encrypt
+                
+                cipher =[]
+                for i in range(len(to_encrypt)):
+                    cipher.append(calc.encrypt(priv, to_encrypt[i]))
+                open("output.txt", "w+").write(str(cipher))
+                print "Your encrypted cipher is: \n", cipher, "\nAnd it's been saved to output.txt"
+                raw_input("Press enter to continue to main menu.")
+                  
+            else:
+                message = open("input.txt", "r").read()
+                print "Your file's message to encrypt is: \n", message
+                to_encrypt = [ord(char) for char in message]
+                print "The ascii representation of your message is:", to_encrypt
+                
+                cipher =[]
+                for i in range(len(to_encrypt)):
+                    cipher.append(calc.encrypt(priv, to_encrypt[i]))
+                open("output.txt", "w+").write(str(cipher))
+                print "Your encrypted cipher is: \n", cipher, "\n And it's been saved to output.txt"
+                raw_input("Press enter to continue to main menu.")  
+#===============================================================================
+        elif choice == 5:
+            try:
+                pub
+            
+            except NameError:
+                p = int(raw_input("Enter a prime number (17, 19, 23, etc): "))
+                q = int(raw_input("Enter another prime number (Not one you entered above): "))
+                print "Generating your public/private keypairs now . . ."
+                pub, priv = calc.keypair(61,53)
+                print "Your public key is: ", pub, " and your private key is: ", priv
+                
+                cipher = open("output.txt", "r").read()
+                print "Your file's cipher to decrypt is: \n", cipher
+                cipher = cipher.split(", ")
+                cipher = [s.strip('[') for s in cipher]
+                cipher = [s.strip(']') for s in cipher]
+                cipher = map(int,cipher)
+                
+                to_decrypt = []
+                for i in range(len(cipher)):
+                    to_decrypt.append(calc.decrypt(pub, cipher[i]))
+                print "The ascii representation of your decrypted cipher is:", to_decrypt
+                print "Your decrypted cipher is:", ''.join(map(chr,map(int, to_decrypt)))
+                raw_input("Press enter to continue to main menu.")
+                
+            else:
+                cipher = open("output.txt", "r").read()
+                print "Your file's cipher to decrypt is: \n", cipher
+                cipher = cipher.split(", ")
+                cipher = [s.strip('[') for s in cipher]
+                cipher = [s.strip(']') for s in cipher]
+                cipher = map(int,cipher)
+                
+                to_decrypt = []
+                for i in range(len(cipher)):
+                    to_decrypt.append(calc.decrypt(pub, cipher[i]))
+                print "The ascii representation of your decrypted cipher is:", to_decrypt
+                print "Your decrypted cipher is:", ''.join(map(chr,map(int, to_decrypt)))
+                raw_input("Press enter to continue to main menu.")  
+#===============================================================================
+        elif choice == 0:
+            loop = False
+        else:
+            raw_input("Wrong option selection. Enter any key to try again.")
--- modules/assets.py
+++ modules/assets.py
@@ -1,5 +1,3 @@
-import random
-
 banner = """
 ###################################################
 ########  ____  ____    _      _  ___  _  #########
@@ -15,7 +13,7 @@ banner = """
 ###################################################
 """
 
-menu1 = """
+menu = """
 --------------------- M E N U ---------------------
 1. Generate new key pair
 2. Encrypt text from input
@@ -24,126 +22,3 @@ menu1 = """
 5. Decrypt text from file
 0. Exit
 """
-
-def is_prime(x):
-    if x == 2:
-        return True
-    if x < 2 or x % 2 == 0:
-        return False
-    for i in range(3, x):
-        if x % i == 0:
-            return False
-    return True
-
-def GCD(a, b):
-    if b>a:
-        while a != 0:
-            b,a = a,b % a
-        return b
-    
-    while b != 0:
-        a,b = b,a % b
-    return a
-
-def multiplicative_inverse(a, mod):
-    a0, mod0 = a, mod
-    x=lasty=0
-    y=lastx=1
-    while (mod!=0):
-        q = a/mod
-        a, mod = mod, a%mod
-        x, lastx = lastx-q*x, x
-        y, lasty = lasty-q*y, y
-    if lastx<0:
-        lastx = mod0+lastx
-    return lastx
-
-def binary_exponentiation(base, power, n):
-    result = 1
-    while power > 0:
-        # If power is even
-        if power % 2 == 0:
-            # Divide the power by 2
-            power = power / 2
-            # Multiply base to itself
-            base = base * base
-        else:
-            # Decrement the power by 1 and make it even
-            power = power - 1
-            # Take care of the extra value that we took out
-            # We will store it directly in result
-            result = result * base
-
-            # Now power is even, so we can follow our previous procedure
-            power = power / 2
-            base = base * base
-    return result%n
-
-def modulus(p, q):
-    # n is the modulus for the public and private keys
-    n = p * q
-    return n
-
-def totient(p, q):
-    # Check p and q are prime
-    if not (is_prime(p) and is_prime(q)):
-        raise ValueError('Both numbers must be prime.')
-    elif p == q:
-        raise ValueError('p and q cannot be equal.')
-
-    # phi(n) is the totient of the modulus n
-    phi = (p-1) * (q-1)
-    return phi
-
-def public_key_exponent(phi):
-    # Condition 1: 1 < e < phi(n)
-    e = random.randrange(1, phi)
-
-    # Contidion 2: e and phi(n) must be coprime
-    g = GCD(e, phi)
-    while g != 1:
-        e = random.randrange(1, phi)
-        g = GCD(e, phi) 
-    return e
-
-def private_key_exponent(phi, e):
-    # Satisfy the congruence relation e*d=1 mod(phi(n))
-    #e = public_key_exponent(phi)
-    d = multiplicative_inverse(e, phi)
-    return d
-
-def public_key(p,q):
-    # (e, n)
-    e = public_key_exponent(totient(p,q))
-    n = modulus(p,q)
-    return (e,n)
-
-def private_key(p, q, pub):
-    # (d, n)
-    e, n = pub
-    #d = private_key_exponent(totient(p,q))
-    d = private_key_exponent(totient(p,q), e)
-    n = modulus(p,q)
-    return (d,n)
-
-def keypair(p, q):
-    pub = public_key(p,q)
-    priv = private_key(p,q, pub)
-    return pub, priv
-#-------------
-
-def encrypt(public_key, message):
-    # Unpack the key into it's components
-    e, n = public_key
-    # Apply exponentiation to each ascii value on the list
-    cipher = pow(message, e, n)
-    #cipher = [(ord(char) ** e) % n for char in msg]
-    return cipher
-
-def decrypt(private_key, cipher):
-    # Unpack the key into it's components
-    d, n = private_key
-    message = pow(cipher, d, n)
-    #msg = [chr((char ** d) % n) for char in cipher]
-    return message
-
